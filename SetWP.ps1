77686a7648ac52207e278f3e6499385d6c114d16
Brandon Sie <33736549+brandonsie@users.noreply.github.com>
2019-12-10 16:12:41 -0500

    Create SetWP.ps1

---
+++ SetWP.ps1
@@ -0,0 +1,24 @@
+# Set wallpaper URL to download
+#$wallpaperURL = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/5424x5424.jpg"
+$wallpaperURL = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/10848x10848.jpg"
+
+# Set download destination (use timestamped filename)
+$wallpaperFile = "C:\Users\brand\Dropbox\wallpaper\wallpaper_$(get-date -f yyyy-MM-dd-HHMM).jpg"
+
+"C:\temp\mybackup $(get-date -f yyyy-MM-dd).zip"
+
+
+# Download and apply wallpaper
+Invoke-WebRequest -Uri $wallpaperURL -OutFile $wallpaperFile
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $wallpaperFile
+
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
