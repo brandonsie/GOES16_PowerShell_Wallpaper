121c3de44c6388ca94e8bb63392b77d2eccedc02
brandonsie <brandonsie@gmail.com>
2021-12-27 21:03:19 -0500

    added user input prompt to download geocolor or daycloudphase

--- Run_Manually/runwp.bat
+++ Run_Manually/runwp.bat
@@ -1,15 +1,27 @@
 @ECHO OFF
 
+:: Prompt for user input
+set /p WPINPUT="Press 1 for Geocolor, 2 for DayCloudPhase: " || set WPINPUT==1
 
+if %WPINPUT%== 2 (
+  set WPTYPE=DayCloudPhase
+) else (
+  set WPTYPE=GEOCOLOR
+) 
+echo Downloading %WPTYPE%
+
+
+:: timeout 10000
 
 :: Download Wallpaper.
 set "dbdir=C:\Users\%USERNAME%\Dropbox\"
 set "scrdir=%dbdir%3_Programming\GOES16_PowerShell_Wallpaper\Run_Manually/"
 set "imgdir=%dbdir%Photos\Wallpaper\"
 
-set "wpurl=https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/1808x1808.jpg"
-set "wppath=%imgdir%wallpaper.jpg"
+set "wpurl=https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/%WPTYPE%/1808x1808.jpg"
 
+set "wppath=%imgdir%wallpaper.jpg"%
+%
 
 PowerShell.exe -Command %scrdir%"dlwp.ps1" -wpurl %wpurl% -wppath %wppath%
 
